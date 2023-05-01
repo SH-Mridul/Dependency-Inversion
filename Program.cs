1110beb67b3c4d725f288bebe026a8fb6a404901
mridulhossain <mridulcse8@gmail.com>
2023-05-02 00:47:56 +0600

    Dependency Inversion

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DependencyInversionPrinciple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          new CustomerBl().getCustomerNameBl();
+        }
+    }
+
+    public class CustomerDataAccessFactory
+    {
+        public static ICustomerDataAccess GetCustomerName()
+        {
+            return new CustomerDataAccess();
+        }
+    }
+
+    public interface ICustomerDataAccess
+    {
+        public string getCustomerNameDataAccess();
+    }
+
+    public class CustomerDataAccess : ICustomerDataAccess
+    {
+        public CustomerDataAccess() { }
+        public string getCustomerNameDataAccess()
+        {
+            return "customer name from data access";
+        }
+    }
+
+
+    public class CustomerBl
+    {
+        public CustomerBl(){}
+
+        public void getCustomerNameBl()
+        {
+            ICustomerDataAccess customerDataAccess = CustomerDataAccessFactory.GetCustomerName();
+            Console.WriteLine(customerDataAccess.getCustomerNameDataAccess());
+        }
+    }
+}
